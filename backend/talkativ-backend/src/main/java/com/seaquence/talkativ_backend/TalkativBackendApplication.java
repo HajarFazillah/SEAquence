a48c348af37ea56271abba0cbd45220fbc360df2
HajarFazillah <hajar.fazillah@gmail.com>
2026-01-24 20:07:36 +0900

    Add initial backend setup with Spring Boot and Gradle configuration

---
+++ backend/talkativ-backend/src/main/java/com/seaquence/talkativ_backend/TalkativBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seaquence.talkativ_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkativBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkativBackendApplication.class, args);
+	}
+
+}
---
+++ backend/talkativ-backend/src/test/java/com/seaquence/talkativ_backend/TalkativBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seaquence.talkativ_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkativBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
